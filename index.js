358c61a354977c4177206d9d743cd4079865b2e5
NurulIslam-EEE <ni163034@gmail.com>
2022-01-26 05:04:02 +0600

    adding blog

--- index.js
+++ index.js
@@ -19,135 +19,116 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const ridersCollection = client.db("hero_rider").collection("riders");
+        const blogsCollection = client.db("end-game").collection("blogs");
 
 
 
-        app.post('/rider', async (req, res) => {
-            // console.log(req.body)
-            console.log(req.files)
-            const name = req.body.name;
-            const email = req.body.email;
-            const age = req.body.age;
-            const address = req.body.address;
-            const phone = req.body.phone;
-            const area = req.body.area;
-            const carName = req.body.carName;
-            const model = req.body.model;
-            const plate = req.body.plate;
-            const pass1 = req.body.pass1;
-            const pass2 = req.body.pass2;
-            const type = req.body.type;
-            const role = req.body.role;
+        // app.post('/rider', async (req, res) => {
+        //     // console.log(req.body)
+        //     console.log(req.files)
+        //     const name = req.body.name;
+        //     const email = req.body.email;
+        //     const age = req.body.age;
+        //     const address = req.body.address;
+        //     const phone = req.body.phone;
+        //     const area = req.body.area;
+        //     const carName = req.body.carName;
+        //     const model = req.body.model;
+        //     const plate = req.body.plate;
+        //     const pass1 = req.body.pass1;
+        //     const pass2 = req.body.pass2;
+        //     const type = req.body.type;
+        //     const role = req.body.role;
 
-            const licence = req.files.licence;
-            const nid = req.files.nid;
-            const profile = req.files.profile;
+        //     const licence = req.files.licence;
+        //     const nid = req.files.nid;
+        //     const profile = req.files.profile;
 
-            const licenceData = licence.data;
-            const nidData = nid.data;
-            const profileData = profile.data;
+        //     const licenceData = licence.data;
+        //     const nidData = nid.data;
+        //     const profileData = profile.data;
 
-            const encodeLicence = licenceData.toString('base64');
-            const encodeNid = nidData.toString('base64');
-            const encodeProfile = profileData.toString('base64');
+        //     const encodeLicence = licenceData.toString('base64');
+        //     const encodeNid = nidData.toString('base64');
+        //     const encodeProfile = profileData.toString('base64');
 
-            const licenceBuffer = Buffer.from(encodeLicence, 'base64');
-            const nidBuffer = Buffer.from(encodeNid, 'base64');
-            const profileBuffer = Buffer.from(encodeProfile, 'base64');
-            const rider = {
-                name,
-                email,
-                age,
-                address,
-                phone, area,
-                carName,
-                model,
-                plate,
-                pass1,
-                pass2,
-                type,
-                role,
-                licence: licenceBuffer,
-                nid: nidBuffer,
-                profile: profileBuffer
-            }
-            const result = await ridersCollection.insertOne(rider);
-            res.json(result)
-        })
+        //     const licenceBuffer = Buffer.from(encodeLicence, 'base64');
+        //     const nidBuffer = Buffer.from(encodeNid, 'base64');
+        //     const profileBuffer = Buffer.from(encodeProfile, 'base64');
+        //     const rider = {
+        //         name,
+        //         email,
+        //         age,
+        //         address,
+        //         phone, area,
+        //         carName,
+        //         model,
+        //         plate,
+        //         pass1,
+        //         pass2,
+        //         type,
+        //         role,
+        //         licence: licenceBuffer,
+        //         nid: nidBuffer,
+        //         profile: profileBuffer
+        //     }
+        //     const result = await ridersCollection.insertOne(rider);
+        //     res.json(result)
+        // })
 
 
         // learner 
-        app.post('/learner', async (req, res) => {
+        app.post('/addBlog', async (req, res) => {
             // console.log(req.body)
             console.log(req.files)
-            const name = req.body.name;
-            const email = req.body.email;
-            const age = req.body.age;
+            const title = req.body.title;
             const address = req.body.address;
-            const phone = req.body.phone;
+            const cost = req.body.cost;
+            const category = req.body.category;
+            const traveler = req.body.traveler;
+            const description = req.body.description;
 
-            const pass1 = req.body.pass1;
-            const pass2 = req.body.pass2;
-            const type = req.body.type;
-            const role = req.body.role;
-
-
-            const nid = req.files.nid;
-            const profile = req.files.profile;
-
-
-            const nidData = nid.data;
-            const profileData = profile.data;
-
-
-            const encodeNid = nidData.toString('base64');
-            const encodeProfile = profileData.toString('base64');
-
-
-            const nidBuffer = Buffer.from(encodeNid, 'base64');
-            const profileBuffer = Buffer.from(encodeProfile, 'base64');
-            const rider = {
-                name,
-                email,
-                age,
+            const img = req.files.img;
+            const imgData = img.data;
+            const encodeImg = imgData.toString('base64');
+            const imgBuffer = Buffer.from(encodeImg, 'base64');
+            const blog = {
+                title,
                 address,
-                phone,
+                cost,
+                category,
+                traveler,
+                description,
 
-                pass1,
-                pass2,
-                type,
-                role,
-
-                nid: nidBuffer,
-                profile: profileBuffer
+                img: imgBuffer
             }
-            const result = await ridersCollection.insertOne(rider);
+            const result = await blogsCollection.insertOne(blog);
             res.json(result)
+            console.log(blog)
         })
-        app.get('/users', async (req, res) => {
-            const cursor = ridersCollection.find({});
-            const result = await cursor.toArray();
-            res.json(result)
-        })
-        app.get('/profile/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email }
-            const result = await ridersCollection.findOne(query);
-            console.log('user', result)
-            res.json(result)
-        })
-        app.get('/admin/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email }
-            const cursor = await ridersCollection.findOne(query);
-            let isAdmin = false;
-            if (cursor.role === 'admin') {
-                isAdmin = true
-            }
-            console.log(cursor)
-            res.json({ admin: isAdmin })
-        })
+        // app.get('/users', async (req, res) => {
+        //     const cursor = ridersCollection.find({});
+        //     const result = await cursor.toArray();
+        //     res.json(result)
+        // })
+        // app.get('/profile/:email', async (req, res) => {
+        //     const email = req.params.email;
+        //     const query = { email: email }
+        //     const result = await ridersCollection.findOne(query);
+        //     console.log('user', result)
+        //     res.json(result)
+        // })
+        // app.get('/admin/:email', async (req, res) => {
+        //     const email = req.params.email;
+        //     const query = { email: email }
+        //     const cursor = await ridersCollection.findOne(query);
+        //     let isAdmin = false;
+        //     if (cursor.role === 'admin') {
+        //         isAdmin = true
+        //     }
+        //     console.log(cursor)
+        //     res.json({ admin: isAdmin })
+        // })
     }
     finally {
         // await client.close();
@@ -164,7 +145,7 @@ async function run() {
 run().catch(console.dir)
 
 app.get('/', (req, res) => {
-    res.send('Hero Rider!')
+    res.send('end game!')
 })
 
 app.listen(port, () => {
